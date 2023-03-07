9fd33f01aa9728949fb15e258e69c08a93db1c33
virajjiwane <vjiwane27@gmail.com>
2023-03-07 17:20:59 +0530

    zookeeper kafka

--- app/config.py
+++ app/config.py
@@ -1,7 +1,12 @@
 import asyncio
+import os
 
 # env Variable
-KAFKA_BOOTSTRAP_SERVERS= "localhost:9093"
+# KAFKA_HOST: str = os.getenv("KAFKA_HOST")
+# KAFKA_PORT: str = os.getenv("KAFKA_PORT")
+# KAFKA_BOOTSTRAP_SERVERS= os.getenv("KAFKA_BOOTSTRAP_SERVERS")
+KAFKA_BOOTSTRAP_SERVERS="localhost:9093"
+print(f"FAST API KAFKA SETTINGS: {KAFKA_BOOTSTRAP_SERVERS}")
 KAFKA_TOPIC="claim"
 KAFKA_CONSUMER_GROUP="group-id"
 loop = asyncio.get_event_loop()
