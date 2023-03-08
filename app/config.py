b09fb7451e1f01a99083e1c444ee95506a350a72
virajjiwane <vjiwane27@gmail.com>
2023-03-08 08:41:48 +0530

    fixed connecting fastapi container with kafka container

--- app/config.py
+++ app/config.py
@@ -1,12 +1,12 @@
 import asyncio
 import os
+import logging
+log = logging.getLogger("uvicorn")
 
 # env Variable
-# KAFKA_HOST: str = os.getenv("KAFKA_HOST")
-# KAFKA_PORT: str = os.getenv("KAFKA_PORT")
-# KAFKA_BOOTSTRAP_SERVERS= os.getenv("KAFKA_BOOTSTRAP_SERVERS")
-KAFKA_BOOTSTRAP_SERVERS="localhost:9093"
-print(f"FAST API KAFKA SETTINGS: {KAFKA_BOOTSTRAP_SERVERS}")
-KAFKA_TOPIC="claim"
+KAFKA_HOST: str = os.getenv("KAFKA_HOST")
+KAFKA_PORT: str = os.getenv("KAFKA_PORT")
+KAFKA_BOOTSTRAP_SERVERS=f"{KAFKA_HOST}:{KAFKA_PORT}"
+KAFKA_TOPIC: str = os.getenv("KAFKA_TOPIC")
 KAFKA_CONSUMER_GROUP="group-id"
 loop = asyncio.get_event_loop()
